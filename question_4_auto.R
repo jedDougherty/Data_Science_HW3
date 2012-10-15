7828be494b2406c155893ff4e80a6bac3cc43bba
JedDougherty <jed2153@columbia.edu>
2012-10-15 02:46:07 -0400

    Minor
    Fixed wording on prompt

--- question_4_auto.R
+++ question_4_auto.R
@@ -9,13 +9,13 @@ file.path <- "http://getglue-data.s3.amazonaws.com/getglue_sample.tar.gz"
 con <- gzcon(url(file.path))
 n=0
 while(n < 1 ){
-  n <- readline("enter a positive integer: ")
+  n <- readline("enter the number of rows you want: ")
   n <- ifelse(grepl("\\D",n),-1,as.integer(n))
   if(is.na(n)){break}  # breaks when hit enter
 }
 }
 {
-n.lines.to.read <- n
+n.lines.to.read <- n + 1
 tmp <- readLines(con, n.lines.to.read)
 articles <- data.frame()
 for(i in 2:length(tmp)){
