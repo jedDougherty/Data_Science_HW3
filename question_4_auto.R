d81478a2c7081e01b72b1afe45d44ce367266076
JedDougherty <jed2153@columbia.edu>
2012-10-15 02:35:58 -0400

    Forgot to do this earlier.
    Added a second version that allows you to enter the number of rows you
    want in the command line
    pretty baller….
    
    To run it make sure you set your working directory to your git
    repository with
    setwd()
    
    Then run:
    source("question_4_auto.R")
    
    and put in the number of rows you want to view when prompted.

--- question_4_auto.R
+++ question_4_auto.R
@@ -4,17 +4,24 @@ install.packages("plyr")
 library("RJSONIO")
 library("plyr")
 #library("rjson")
-
+{
 file.path <- "http://getglue-data.s3.amazonaws.com/getglue_sample.tar.gz"
 con <- gzcon(url(file.path))
-
-n.lines.to.read <- 1000 #Put in how many lines to read here.
+n=0
+while(n < 1 ){
+  n <- readline("enter a positive integer: ")
+  n <- ifelse(grepl("\\D",n),-1,as.integer(n))
+  if(is.na(n)){break}  # breaks when hit enter
+}
+}
+{
+n.lines.to.read <- n
 tmp <- readLines(con, n.lines.to.read)
 articles <- data.frame()
-for(i in 2:length(tmp)){ #Have to set it to 2 because first line is stupid title
+for(i in 2:length(tmp)){
   json_data <- fromJSON(paste(tmp[i], collapse=""))
   json_data <- as.data.frame(json_data)
-  articles <- rbind.fill(articles, json_data) #This does automatically what Eurry programmed last time
+  articles <- rbind.fill(articles, json_data)
 }
 
 View(articles)
