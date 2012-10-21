f7bec38a50040860e1509ad81a3155673b8682f7
pivota-casebook <jed@casecommons.org>
2012-10-20 20:37:48 -0400

    Added code for all 2011 articles
    Yep that is pretty much it. It is still pretty slow :( Takes a couple
    hours to do one million lines.

--- question_4.R
+++ question_4.R
@@ -10,29 +10,30 @@ library("data.table")
 file.path <- "http://getglue-data.s3.amazonaws.com/getglue_sample.tar.gz"
 con <- gzcon(url(file.path))
 
-n.lines.to.read <- 1000 #Put in how many lines to read here.
+n.lines.to.read <- 10 #Put in how many lines to read here.
 tmp <- readLines(con, n.lines.to.read)
-articles <- data.frame()
+listings <- data.frame()
 for(i in 2:length(tmp)){ #Have to set it to 2 because first line is stupid title
   json_data <- fromJSON(paste(tmp[i], collapse=""))
   json_data <- as.data.frame(t(json_data))
-  articles <- rbind.fill(articles, json_data) #This does automatically what Eurry programmed last time
+  listings <- rbind.fill(listings, json_data) #This does automatically what Eurry programmed last time
 }
 
-View(articles)
+View(listings)
 
 #Finds Unique actions in the data
-actions <- unique(articles$action)
+actions <- unique(listings$action)
 
 #Finds number of these unique actions
-action_table <- table(articles$action)
+action_table <- table(listings$action)
 
 #Top Ten Movies
-DT <- as.data.table(articles)
+DT <- as.data.table(listings)
 setkey(DT,title)
 DT <- DT[modelName == "movies",]
 grabber <- DT[,sum((action == "Liked")-(action == "Disliked")),by=title]
 grabber_sorted <- grabber[order(V1)]
 
-#Top
+#Number of actions in 2011
+2011_articles <- length(grep("^2011",listings$timestamp,value=TRUE))
 
