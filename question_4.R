b9ef222ec957052b03a9d88d833cd7020d3c9ced
jedDougherty <jeddougherty@dyn-160-39-205-72.dyn.columbia.edu>
2012-10-23 12:00:56 -0400

    Added 4a and the graphs

--- question_4.R
+++ question_4.R
@@ -1,27 +1,30 @@
 install.packages("RJSONIO")
 install.packages("plyr")
 install.packages("data.table")
-#install.packages("rjson")
+install.packages("rjson")
 library("RJSONIO")
 library("plyr")
 library("data.table")
-#library("rjson")
+library("rjson")
 
 file.path <- "http://getglue-data.s3.amazonaws.com/getglue_sample.tar.gz"
 con <- gzcon(url(file.path))
 
 n.lines.to.read <- 1000 #Put in how many lines to read here.
 tmp <- readLines(con, n.lines.to.read)
-listings <- list()
-a<-system.time(for(i in 2:n.lines.to.read){ #Have to set it to 2 because first line is stupid title
+
+a<-system.time(theRead <- lapply(tmp[-1], rjson::fromJSON))
+b<-system.time(theData <-ldply(theRead, as.data.frame))
+View(theData)
+
+listings_little <- list()
+c<-system.time(for(i in 2:n.lines.to.read){ #Have to set it to 2 because first line is stupid title
   json_data <- fromJSON(paste(tmp[i], collapse=""))
   # listings <- rbind.fill(listings, json_data) #This does automatically what Eurry programmed last time
-  listings[[i]] <- as.data.frame(t(json_data))
+  listings_little[[i]] <- as.data.frame(t(json_data))
 })
+d<-system.time(listings_little <- rbind.fill(listings_little))
 close(con)
-
-b<-system.time(listings <- rbind.fill(listings))
-
 
 a+b
 
--- question_4_large_sample.R
+++ question_4_large_sample.R
@@ -12,50 +12,57 @@ library("data.table")
   listings_onek <- list()
   a<-system.time(for(i in 2:n.lines.to.read){ #Have to set it to 2 because first line is stupid title
     json_data <- fromJSON(paste(tmp[i], collapse=""))
-   # listings <- rbind.fill(listings, json_data) #This does automatically what Eurry programmed last time
+   # listings_onek <- rbind.fill(listings_onek, json_data) #This does automatically what Eurry programmed last time
     listings_onek[[i]] <- as.data.frame(t(json_data))
   })
   a
   b<-system.time(listings_onek <- rbind.fill(listings_onek))
-  #rbindlist.data.frame <- rbindlist(listings_onek)
+  #rbindlist.data.frame <- rbindlist(listings_onek_onek)
   b
   a+b
 #Finds Unique actions in the data
-actions <- unique(listings$action)
+actions <- unique(listings_onek$action)
 
 #Finds the number of Unique users in the data
-users <- unique(listings$userId)
+users <- unique(listings_onek$userId)
 
 #Finds number of these unique actions
-action_table <- table(listings$action)
+action_table <- table(listings_onek$action)
+
+#We'll use the data.table format for the rest of our 
+#manipulation
+DT <- as.data.table(listings_onek)
+
 
 #Top Ten Movies
-DT <- as.data.table(listings)
+
 setkey(DT,title)
-DT <- DT[modelName == "movies",]
-grabber <- DT[,sum((action == "Liked")-(action == "Disliked")),by=title]
+DT_movies <- DT[modelName == "movies",]
+grabber <- DT_movies[,sum((action == "Liked")-(action == "Disliked")),by="title"]
 grabber_sorted <- grabber[order(V1)]
 
 #Number of actions in 2011
 #To doublecheck this I opened the entire file in Vim
 #and used the following command
-#:%s/\"timestamp\": \"201-\d\d-\d\d//gn
+#:%s/\"timestamp\": \"2011-\d\d-\d\d//gn
 #This returned 10709526 actions in 2011
 
 #Number of actions in 2011
-year_articles <- length(grep("^2011",listings$timestamp,value=TRUE))
+year_articles <- length(grep("^2011",listings_onek$timestamp,value=TRUE))
 year_articles * 19831300/n.lines.to.read
 #This gives us a very similar number, which backs up the idea that we have
 #a good random sample.
 
 #5 new questions:
-#What movies did regular users love? Are they different from one time users?
-#Regular users defined as appearing on the site 3 or more times
-PU <- as.data.table(listings)
-setkey(PU,userId)
-grabber <- PU[,length(userId),by=userId]
+#What movies did power users love? Are they different from one time users?
+#Regular users defined as the 100 people with the most user name appearances
+setkey(DT,userId)
+DT_users <- DT[,length(timestamp),by=userId]
+DT_users <- DT_users[order(V1)]
+power_users <- tail(DT_users,100)
+DT_movie_users <- DT[userId == power_users$userId,]
+grabber <- DT_movies[,sum((action == "Liked")-(action == "Disliked")),by="title"]
 grabber_sorted <- grabber[order(V1)]
-
 #Did people without numbers in their user names
 #have better taste in movies? Using rotten_tomatoes 
 #api, lets check it out. (I've long held the 
